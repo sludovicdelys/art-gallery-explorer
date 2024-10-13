9bc936fc7a1d7c42540f8a851e072dbe0ab15766
Sabrina Ludovic de Lys <ludovicdelyssabrina@gmail.com>
2024-10-13 11:24:18 +0200

    perf : remove jest testing & create api utility

---
+++ utils/api.ts
@@ -0,0 +1,37 @@
+const API_KEY = process.env.API_KEY;
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+export interface Artwork {
+    id: string;
+    objectNumber: string;
+    title: string;
+    principalOrFirstMaker: string;
+    webImage: {
+      url: string;
+    };
+}
+  
+export interface ArtworkDetails extends Artwork {
+    description: string;
+    dating: {
+        presentingDate: string;
+    };
+}
+  
+export async function fetchArtworks(page: number = 1, pageSize: number = 10): Promise<{ artObjects: Artwork[] }> {
+    const url = `${BASE_URL}/collection?key=${API_KEY}&imgonly=true&p=${page}&ps=${pageSize}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error('Failed to fetch artworks');
+    }
+    return response.json();
+}
+  
+export async function fetchArtworkDetails(objectNumber: string): Promise<{ artObject: ArtworkDetails }> {
+    const url = `${BASE_URL}/collection/${objectNumber}?key=${API_KEY}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error('Failed to fetch artwork details');
+    }
+    return response.json();
+}
